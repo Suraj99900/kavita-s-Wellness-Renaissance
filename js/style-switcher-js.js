787f7a418a39b030d2301bd9f9e7963b24cee520
suraj jaiswal <suraj.jaiswal@plus91.in>
2024-01-09 08:41:41 +0530

    added programe

--- js/style-switcher-js.js
+++ js/style-switcher-js.js
@@ -21,7 +21,8 @@ function setActivityStyle(color) {
         else {
             style.setAttribute("disabled", "true");
         }
-    })
+    });
+    sessionStorage.setItem('theme-text-color', color);
 }
 
 // ================= theme light and dark mode
@@ -32,12 +33,33 @@ dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
-})
+
+    // Store theme preference in sessionStorage
+    const isDarkMode = document.body.classList.contains("dark");
+    sessionStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+
+});
 
 window.addEventListener("load", () => {
-    if (document.body.classList.contains("dark")) {
+    // Retrieve theme preference from sessionStorage
+    const savedTheme = sessionStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
         dayNight.querySelector("i").classList.add("fa-sun");
+        document.body.classList.toggle("dark");
     } else {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
-})
+
+    if(sessionStorage.getItem('theme-text-color') != ''){
+        const alternateStyle = document.querySelectorAll(".alternate-style");
+        alternateStyle.forEach((style) => {
+            if (sessionStorage.getItem('theme-text-color') == style.getAttribute("title")) {
+                style.removeAttribute("disabled");
+            }
+            else {
+                style.setAttribute("disabled", "true");
+            }
+        });
+    }
+});
